f1be8c6df2ebd7f6043991f497b32ab29bdae5e5
Ankit Raj <ankit.raj928@gmail.com>
2023-12-16 15:56:14 +0000

    sentry feedbackl

--- sentry.config.js
+++ sentry.config.js
@@ -1,14 +1,26 @@
 import * as Sentry from "@sentry/gatsby"
 
 Sentry.init({
-  dsn:
-    "https://0531e86b78734b538a778980abd13722@o1258362.ingest.sentry.io/4505099568873472",
+  dsn: process.env.GATSBY_SENTRY_DSN,
   integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
   // Performance Monitoring
   tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+
+  integrations: [
+    new Sentry.Feedback({
+      // Additional SDK configuration goes in here
+      colorScheme: "light",
+     }),
+     new BrowserTracing(),
+     new Sentry.Replay({
+      // Additional SDK configuration goes in here, for example:
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
+  ],
 
   beforeSend: event => {
     // Check if it is an exception, and if so, show the report dialog
